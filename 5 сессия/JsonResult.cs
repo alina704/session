44ab23ae370192594b9bd8c906d2c5e52846955c
Евгений Иванков <58434271+Squzik@users.noreply.github.com>
2021-12-26 12:49:17 +0300

    Add files via upload

---
+++ 5 сессия/JsonResult.cs
@@ -0,0 +1,11 @@
+﻿namespace WindowsApp.Modules
+{
+    class JsonResult
+    {
+        public int id { get; set; } // Код штрафа        
+        public string car_num { get; set; } // Номер авто        
+        public string licence_num { get; set; } // Номер в/у      
+        public string create_date { get; set; } // Дата   
+        public string photo { get; set; } // Ссылка на фото
+    }
+}
---
+++ 5 сессия/MainForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using WindowsApp.Database;
+using WindowsApp.Modules;
+
+
+namespace WindowsApp.Forms
+{
+
+    /// <summary>
+    /// Defines the <see cref="MainForm" />.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+
+        /// <summary>
+        /// Срабатывает при нажатии на кнопку "GET".
+        /// Отправляет GET запросы и стягивает картинки.
+        /// </summary>
+        /// <param name="sender">.</param>
+        /// <param name="e">.</param>
+        private void TryToGetClick(object sender, EventArgs e)
+        {
+            JArray data = (JArray)API.GetFines(partBox.Text, modifedDate.Value.ToString())["data"];
+            ExportFines(data);
+            response.Text = data.ToString();
+            FinesSummary.DataSource = data;
+            FinesSummary.Columns[0].Name = "Номер";
+            FinesSummary.Columns[1].Name = "Гос.номер";
+            FinesSummary.Columns[2].Name = "Вод. удостоверение";
+            FinesSummary.Columns[3].Name = "Дата";
+            FinesSummary.Columns[4].Name = "Фото";
+        }
+
+    }
+}
