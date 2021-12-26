0dd66b8a674f50e246884f9f78a6f2c49b8d0a22
Евгений Иванков <58434271+Squzik@users.noreply.github.com>
2021-12-26 12:48:16 +0300

    Add files via upload

---
+++ 3 сессия/Driver.cs
@@ -0,0 +1,41 @@
+﻿/// <summary>
+/// Отрисовка карточки водительского удостоверения и отображение её в driverLicenseImage.
+/// </summary>
+/// <param name="sender"></param>
+/// <param name="e"></param>
+private void DriverLicensePaintClick(object sender, EventArgs e)
+{
+    int id = drivers_LicsDataGridView.CurrentCell.RowIndex + 1;
+    // Подгрузка данных из БД.
+    drivers current = db.drivers.SqlQuery($"select * from drivers where id = {id}").AsNoTracking().FirstOrDefaultAsync().Result;
+    licences license = db.licences.SqlQuery($"select * from licences where id = {id}").AsNoTracking().FirstOrDefaultAsync().Result;
+    // Отрисовка изображения.
+    DriverLicenseImage img = new DriverLicenseImage(Properties.Resources.driver_license_template);
+    img.DrawInfo(current.lastname, current.firstname, current.middlename, license.categories, current.address,
+                 license.licence_date.ToShortDateString(), license.expire_date.ToShortDateString(), license.licence_number.ToString(), license.licence_series);
+    driverLicenseImage.Image = img.GetBitmap();  // Подгрузка в pictureBox.
+    driverLicenseSaveImageButton.Enabled = true;
+}
+/// <summary>
+/// Вызывает <see cref="SaveFileDialog"/> для сохранения карточки водителя в файл.
+/// </summary>
+/// <param name="sender"></param>
+/// <param name="e"></param>
+private void DriverLicenseSaveImage(object sender, EventArgs e)
+{
+    SaveFileDialog dialog = new SaveFileDialog();
+    Stream stream;
+    dialog.Filter = "image files (*.jpg)|*.jpg";
+    dialog.FilterIndex = 2;
+    dialog.RestoreDirectory = true;
+
+    if (dialog.ShowDialog() == DialogResult.OK)
+    {
+        if ((stream = dialog.OpenFile()) != null)
+        {
+            byte[] data = DriverLicenseImage.GetBytes(driverLicenseImage.Image);
+            stream.Write(data, 0, data.Length);
+            stream.Close();
+        }
+    }
+}
---
+++ 3 сессия/DriverLicenseImage.cs
@@ -0,0 +1,95 @@
+﻿using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace WindowsApp.Modules
+{
+    class DriverLicenseImage
+    {
+        private Bitmap bitmap;
+        private Graphics graphics;
+        private Font font;
+        private Brush brush;
+        private Brush hintBrush;
+        private Brush numBrush;
+
+        /// <summary>
+        /// Создает картинку для дальнейшей работы с ней.
+        /// </summary>
+        /// <param name="filename"></param>
+        public DriverLicenseImage(Bitmap bmp)
+        {
+            bitmap = new Bitmap(bmp);
+            graphics = Graphics.FromImage(bitmap);
+            font = new Font("Arial", 24);
+            brush = new SolidBrush(Color.Black);
+            hintBrush = new SolidBrush(Color.Gray);
+            numBrush = new SolidBrush(Color.DarkViolet);
+            SetupGraphics();
+        }
+
+        /// <summary>
+        /// Рисует данные ВУ.
+        /// помоггите пожалвуйтста 
+        /// </summary>
+        public void DrawInfo(string lastname, string firstname, string middlename, string categories,
+                             string address, string dateFrom, string dateTo, string licNum, string licSerial)
+        {
+            float x = 285;
+            float y = DrawPart("1.", lastname, "SURNAME", x, 200);
+            y = DrawPart("2.", firstname + " " + middlename, "NAME MIDDLE NAME", x, y);
+            y = DrawPart("3.", address, "", x, y);
+            y = DrawPart("4a)", dateFrom + "  " + dateTo, "", x, y);
+            y = DrawPart("5.", licSerial + " " + licNum, "", x, y);
+            DrawPart("9.", categories, "", x, y + 24*4);
+        }
+
+        /// <summary>
+        /// Рисует отдельную строку с подсказкой, чтобы моя шиза была в порядке
+        /// </summary>
+        /// <param name="number">номер строки .. а зачем</param>
+        /// <param name="text">текст строки</param>
+        /// <param name="hint">подсказка снизу</param>
+        /// <param name="x">позиция в X</param>
+        /// <param name="y">позиция в Y</param>
+        /// <returns></returns>
+        public float DrawPart(string number, string text, string hint, float x, float y)
+        {
+            graphics.DrawString(number, font, numBrush, new PointF(x, y));
+            graphics.DrawString(text, font, brush, new PointF(x + font.Size*1.5f, y));
+            graphics.DrawString(hint, font, hintBrush, new PointF(x + font.Size * 1.5f, y + font.Size * 1.2f));
+            return y + font.Size * 2.5f;
+        }
+
+        /// <summary>
+        /// Возвращает bitmap.
+        /// </summary>
+        /// <returns></returns>
+        public Bitmap GetBitmap()
+        {
+            return bitmap;
+        }
+
+        /// <summary>
+        /// Преобразует <see cref="Image"/> в <see cref="byte"/>
+        /// </summary>
+        /// <param name="img"></param>
+        /// <returns></returns>
+        public static byte[] GetBytes(Image img)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                img.Save(stream, ImageFormat.Jpeg);
+                return stream.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Настройка Graphics
+        /// </summary>
+        private void SetupGraphics()
+        {
+            graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+        }
+    }
+}
